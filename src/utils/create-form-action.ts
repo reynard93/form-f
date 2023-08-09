d3e5a0a46397bb7998ab41996e83fa0366726965
ReynardLee <reynard.lee@thoughtworks.com>
2023-08-09 10:57:51 +0800

    refactor createActionsFromProps

--- src/utils/create-form-action.ts
+++ src/utils/create-form-action.ts
@@ -4,33 +4,50 @@ import { ListFieldSchema } from '../../typings/schema'
 import { initialiseFormState } from './init-form-state'
 import { ListAttributes } from '../../typings/list-attribute'
 
+// Function to handle actions for field schema
+function handleFieldSchemaActions(key: string, formState: any, actions: Record<string, any>) {
+  const actionName = `${capitalize(key)}`
+  actions[`set${actionName}`] = function (newValue?: any, listAttributes?: ListAttributes) {
+    if (listAttributes) {
+      formState.value[listAttributes.listId][listAttributes.index][key] = newValue //OWN fieldId
+    } else {
+      formState.value[key] = newValue
+    }
+  }
+}
+
+// Function to handle actions for list schema
+function handleListSchemaActions(
+  key: string,
+  _schemaList: ListFieldSchema,
+  formState: any,
+  errorStates: any,
+  currentIdx: any,
+  actions: Record<string, any>
+) {
+  const actionName = `${capitalize(key)}`
+  actions['addIdx' + actionName] = () => {
+    errorStates.value[key][currentIdx.value] = {}
+    formState.value[key][currentIdx.value] = initialiseFormState(_schemaList.schema)
+  }
+  actions['removeIdx' + actionName] = function (index: number) {
+    if (currentIdx.value - 1 >= _schemaList.min) {
+      formState.value[key].splice(index, 1) // where dont need .value?
+      errorStates.value[key].splice(index, 1)
+    }
+  }
+}
+
 export function createActionsFromProps(schema: any, formState: any, errorStates: any) {
   return Object.keys(schema).reduce((actions, key) => {
-    const actionName = `${capitalize(key)}`
     const currentIdx = computed(() => formState.value[key].length)
     const _schema = schema[key]
 
     if (isFieldSchema(_schema)) {
-      actions[`set${actionName}`] = function (newValue?: any, listAttributes?: ListAttributes) {
-        if (listAttributes) {
-          formState.value[listAttributes.listId][listAttributes.index][key] = newValue //OWN fieldId
-        } else {
-          formState.value[key] = newValue
-        }
-      }
+      handleFieldSchemaActions(key, formState, actions)
     } else {
       const _schemaList = _schema as ListFieldSchema
-      actions['addIdx' + actionName] = () => {
-        // errorStates needs to be b4 formState, else currentIdx not synced with formState
-        errorStates.value[key][currentIdx.value] = {}
-        formState.value[key][currentIdx.value] = initialiseFormState(_schemaList.schema)
-      }
-      actions['removeIdx' + actionName] = function (index: number) {
-        if (currentIdx.value - 1 >= _schemaList.min) {
-          formState.value[key].splice(index, 1) // where dont need .value?
-          errorStates.value[key].splice(index, 1)
-        }
-      }
+      handleListSchemaActions(key, _schemaList, formState, errorStates, currentIdx, actions)
       actions = {
         ...actions,
         ...createActionsFromProps(_schemaList.schema, formState, errorStates)
